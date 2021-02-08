1d4b3ee23efb4469b8b785195d62f6d65d752b7a
Galla Sravani <70489971+sravsgits@users.noreply.github.com>
2021-02-08 09:42:16 +0530

    Create day1-data-types.py

---
+++ day1-data-types.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+m=0
+n=0.0000
+k=''
+# Read and save an integer, double, and String to your variables.
+m=int(raw_input())
+n=float(raw_input())
+k=str(raw_input())
+# Print the sum of both integer variables on a new line.
+print(i+m)
+# Print the sum of the double variables on a new line.
+print(d+n)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+k)
