dff718d6d544d7458c554653eb25f274c62004d9
Galla Sravani <70489971+sravsgits@users.noreply.github.com>
2021-02-13 23:06:50 +0530

    Create day5-loops.py

---
+++ day5-loops.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(1,11):
+        print("{0} x {1} = {2}".format(n,i,n*i))
